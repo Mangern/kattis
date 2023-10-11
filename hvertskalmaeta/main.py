d6c2621ef3b026485a42935af635a3f81f78136a
Magnus <magnuseifr@gmail.com>
2023-10-11 11:47:51 +0200

    hvertskalmaeta

---
+++ hvertskalmaeta/main.py
@@ -0,0 +1,13 @@
+reyk = {
+    "Reykjavik",
+    "Kopavogur",
+    "Hafnarfjordur",
+    "Reykjanesbaer",
+    "Gardabaer",
+    "Mosfellsbaer",
+    "Arborg",
+    "Akranes",
+    "Seltjarnarnes",
+}
+
+print("Reykjavik" if input() in reyk else "Akureyri")
