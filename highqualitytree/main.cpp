4fb1250accbde19fbf1af304775d04446f9c1505
Magnus <magnuseifr@gmail.com>
2023-10-15 15:39:47 +0200

    highqualitytree

---
+++ highqualitytree/main.cpp
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+template<typename t, size_t N>
+using ar = array<t,N>;
+using ii = ar<ll,2>;
+using vii = vector<ii>;
+using ld = long double;
+
+#define all(v) begin(v), end(v)
+
+const int INF = numeric_limits<int>::max();
+const ll INFLL = numeric_limits<ll>::max();
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {
+	return (unsigned ll)rng() % B;
+}
+template<typename t>
+istream& operator >> (istream& in, vector<t>& vec) {
+    for (t& x : vec)in >> x;
+    return in;
+}
+
+template<typename t>
+ostream& operator << (ostream& out, vector<t>& vec) {
+    int n = (int)vec.size();
+    for (int i = 0; i < n; ++i) {
+        out << vec[i];
+        if (i < n - 1)out << " ";
+    }
+    return out;
+}
+
+template<typename t>
+t min(const vector<t>& vec) {
+    t ans = vec[0];
+    for (const auto& el : vec) {
+        ans = min(ans, el);
+    }
+    return ans;
+}
+
+template<typename t>
+t max(const vector<t>& vec) {
+    t ans = vec[0];
+    for (const auto& el : vec) {
+        ans = max(ans, el);
+    }
+    return ans;
+}
+const int mxN = 2e5+7;
+
+void setIO() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void pre() {
+
+}
+
+int n;
+vi adj[mxN];
+int h[mxN];
+int rh[mxN];
+int ans;
+
+void dfs_h(int u, int cf = -1) {
+    vi hs(2, -1);
+    for (int v : adj[u])if (v != cf) {
+        dfs_h(v, u);
+        hs.push_back(h[v]);
+    }
+    sort(all(hs));
+    h[u] = min(hs[hs.size() - 1] + 1, hs[hs.size() - 2] + 2);
+}
+
+void dfs_ans(int u, int cf = -1) {
+    for (int v : adj[u])if (v != cf) {
+        h[v] = min(h[v], h[u]-1);
+        dfs_ans(v, u);
+    }
+}
+
+int main() {
+    setIO();
+    pre();
+
+    cin >> n;
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v, --u, --v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs_h(0);
+
+    memset(rh, -1, sizeof rh);
+
+    dfs_ans(0);
+    
+    for (int u = 0; u < n; ++u) {
+        if (h[u] < 0) {
+            ++ans;
+            //cout << u+1 << " ";
+        }
+    }
+    //cout << endl;
+
+    cout << ans << endl;
+
+    return 0;
+}
