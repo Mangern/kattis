11f867a9ee201dfff929696f7f7038d3d565125b
Magnus <magnuseifr@gmail.com>
2024-10-07 00:07:47 +0200

    bookclub

---
+++ bookclub/main.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+template<typename t, size_t N>
+using ar = array<t, N>;
+using ii = ar<ll, 2>;
+using vii = vector<ii>;
+using vll = vector<ll>;
+
+template<typename t>
+istream& operator>>(istream& in, vector<t>& vec) {
+    for (t& x : vec) {
+        in >> x;
+    }
+    return in;
+}
+
+template<typename t>
+ostream& operator<<(ostream& os, vector<t>& vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        os << vec[i];
+        if (i < vec.size() - 1) os << " ";
+    }
+    return os;
+}
+#define all(v) begin(v), end(v)
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {
+    return (uint64_t)rng() % B;
+}
+
+template<typename t>
+t min(const vector<t>& v) {
+    return *min_element(all(v));
+}
+
+template<typename t>
+t max(const vector<t>& v) {
+    return *max_element(all(v));
+}
+const int INF = numeric_limits<int>::max();
+const ll INFLL = numeric_limits<ll>::max();
+const int mxN = 2e4+6;
+const ll MOD  = 1e9+7;
+
+int n, m;
+vi adj[mxN];
+int vis[mxN];
+int match[mxN];
+
+int aug(int u) {
+    if (vis[u])return 0;
+    vis[u] = 1;
+
+    for (int& v : adj[u])
+        if (match[v] == -1 || aug(match[v])) {
+            match[v] = u;
+            return 1;
+        }
+    return 0;
+}
+
+void testCase() {
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(n+v);
+    }
+    int mcbm = 0;
+    memset(match, -1, sizeof match);
+    for (int i = 0; i < n; ++i) {
+        memset(vis, 0, sizeof vis);
+        mcbm += aug(i);
+    }
+    cout << (mcbm == n ? "YES" : "NO") << endl;
+}
+
+void setIO() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void pre() {
+
+}
+
+int main() {
+    setIO();
+    pre();
+    int t = 1;
+    //cin >> t;
+
+    for (int tc = 1; tc <= t; ++tc) {
+        testCase();
+    }
+    return 0;
+}
