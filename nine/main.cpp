eb16be1a348d53a11d8b7cef2ffdee8a16e1cc9e
Magnus <magnuseifr@gmail.com>
2024-10-09 08:32:06 +0200

    nine

---
+++ nine/main.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+using vi = vector<int>;
+template<typename t, size_t N>
+using ar = array<t, N>;
+using ii = ar<ll, 2>;
+using vii = vector<ii>;
+using vll = vector<ll>;
+
+template<typename t>
+istream& operator>>(istream& in, vector<t>& vec) {
+    for (t& x : vec) {
+        in >> x;
+    }
+    return in;
+}
+
+template<typename t>
+ostream& operator<<(ostream& os, vector<t>& vec) {
+    for (int i = 0; i < vec.size(); ++i) {
+        os << vec[i];
+        if (i < vec.size() - 1) os << " ";
+    }
+    return os;
+}
+#define all(v) begin(v), end(v)
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {
+    return (uint64_t)rng() % B;
+}
+
+template<typename t>
+t min(const vector<t>& v) {
+    return *min_element(all(v));
+}
+
+template<typename t>
+t max(const vector<t>& v) {
+    return *max_element(all(v));
+}
+const int INF = numeric_limits<int>::max();
+const ll INFLL = numeric_limits<ll>::max();
+const int mxN = 2e5+6;
+const ll MOD  = 1e9+7;
+
+ll pw(ll a, ll b) {
+    if (b == 0) return 1;
+    ll r = pw(a,b>>1);
+    r = (r * r) % MOD;
+    if (b&1) r = (r * a) % MOD;
+    return r;
+}
+
+void testCase() {
+    ll d;
+    cin >> d;
+
+    ll ans = (8 * pw(9,d-1))%MOD;
+    cout << ans << endl;
+}
+
+void setIO() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void pre() {
+
+}
+
+int main() {
+    setIO();
+    pre();
+    int t = 1;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; ++tc) {
+        testCase();
+    }
+    return 0;
+}
