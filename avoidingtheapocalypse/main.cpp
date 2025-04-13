9a8acf55afb8de0ef9ed1268d571e9a7482bdc40
Magnus <magnuseifr@gmail.com>
2025-04-13 17:19:28 +0200

    avoidingtheapocalypse

---
+++ avoidingtheapocalypse/main.cpp
@@ -0,0 +1,215 @@
+#include <bits/stdc++.h>
+/*MMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNXXKK0KXXXXXXNNNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMM*/
+using namespace std;/*MMMMMMWKklc:codl:clldxkkxxkkOXNWMMMMMMMMMMMMMMMMMMMMMMMM*/
+using ll=long long;/*MMMMWXkl,...',cddc,,,,:oxkxdooox0NMM*/using ld=long double;
+const int/*MMMMMMMWNXK0kxd:........,cdxdoc,';lxOkxdoc:oK*/INF=numeric_limits<int
+>::max();/*MMMMMW0oc:;,''..........';cdkkxc'',cdkkddoc;dN*/const ll INFLL=/*MM*/
+/*MMMMMMMMMMMMMM0;.............  ....,cdxxo,',:oxkxolooxXM*/numeric_limits<ll>::
+max();using vi/*x..   ......        ..,codo:,,:dO0xoccco0W*/=vector<int>;/*MMM*/
+template</*MMMMWO;.   .. .           ..;:::::;:lxxdolc:cxN*/typename t, /*MMMM*/
+size_t N>/*MMMMMNk:. ...             ..';;:cllloooc:::::lONMMMMMMMMMMMMMMMMMMM*/
+using ar=/*MMMMMWKko:,..            ...,:clllooodoc;,,,,:o0WMMMMMMMMMMMMMMMMMM*/
+array<t,N>;/*MMWXXXKk:..    .     ....':ccllllllllc:,'.',:xN*/using ii=array<ll,
+2>;using vii=/*X0X0kd:.     .    ....';ccllllllllllc:,..':OW*/vector<ii>;/*MMM*/
+using vll=/*MWKxlc,....     .  ......;:ccllllllcc:;;;,,,ckX*/vector<ll>;/*MMMM*/
+template</*MNk;..      ..    .......',;;;::cllcc;,,,,,cldXM*/typename t> istream
+& operator/*d'.       .....  ....',;,'',;;:cllc:;'..,:dkxKM*/>>(istream& in,/**/
+vector<t>/*0c..        ........';:;,...',,:clllc:;;;:oO0xkW*/&vec){for(t& x:vec)
+{in>>x;}/*W0:..         ......';cc:::;::::cllloollccclxOdxW*/return in;}/*MMMM*/
+template</*No'. ...      .....,:cllllcccclllllooolllloO0xKM*/typename t>ostream&
+operator<</*Kxc'....      ..',;:cllolllllllllllooolllxKKXW*/(ostream& os,vector<
+t>& vec){/*MMWN0o,.......:l:l0KkllooooollllllccclllldOXNWMM*/for(int i=0;i< vec.
+size();++i){os<<vec[i];/*NNOlOWWOllooooooollc:;;::coxKWM*/if(i<vec.size()-1)os<<
+" ";}return os;}/*MMMMMMMMMMMMMMKoclloooolc;,'''';lx0NWWWMMWWWWWWWWWMMMMMMMMMM*/
+#define all(v) begin(v), /*MKONWOl:cclllllllc::::lxxdk0KKKKKXXX*/end(v)/*MMMMM*/
+mt19937_64 rng(chrono::/*MMW0O0xc::cccccllllllccclk0dloddddxkkk00K*/steady_clock
+::now().time_since_epoch/*Xkl::;;:ccccccccccc::::cdOklcoxxxdoooodddxkO*/().count
+());ll myRand(ll B){/*WXOxl:;;;;;:ccclcccccc::::::clollodxdolllllllllodk*/return
+(uint64_t)rng()%B;}/*Oxolc::::;;;:cccccccccccccc::cloolccooolllllllllllox0NMMM*/
+template<typename/*dolllc:;;::;:::::::cccc:ccccclllllcc;:looollllllllclloxKM*/t> 
+t min(const/*0kdollllllll:;;::::::;;;:::ccc:::cllllcccc:;cooolccllllccll*/vector
+<t>& v){/*kkxoollllclllolcccllccccccccccccc:;:ccllcclllc:cooollccclcclcc*/return
+*min_element/*llllcclllllc:cllllllllllllllc::ccccclllllc:cloollcccccccccl*/(all(
+v));}/*W0olllllllllclllllc::ccllllllllllllllllllllllllllclloollc:cccccccclkNMM*/
+template/*olllllllllllolol::ccllllllllllllllllllllllllllllcldolc::ccccccclOWM*/<
+typename/*oollllcllllloooocccllllllllllllllllllllcccllllolclddoc;:ccccccclOW*/t>
+t max(/*Kdoolllllllllloooolcclllllloolllllllllcccccclllooolloddl:;:cccccl*/const
+vector</*doollllllllllooooolccllloooooollllllcccccclllooooolodddl::cccccldKK*/t>
+& v){/*MXxooolllllcccclooooollllooooddoooolccc::cccllooodddoodddoc:ccccc*/return
+*max_element(/*lllcc:;cooooooooooddxkOkxddolc:::cclllooooddddddddocccllllx*/all(
+v));}/*MW0ooooollllcc;;loodooodxk0KXNNNXKOdl:;;:cclllloooooooddddolcccc:oKWMMMMM
+MMMMMMMMMXxllooollllc::looddddk0KXNNWWWNNKxoc,,:cllllllooooooddddolc::::xNMMMMMM
+MMMMMMMMMWKdoloollllc:cooodxkO0KXXXNNNNNNKxol;,cllllllllllllodoooolcc::cOWMMMMMM
+MMMMMMMMMMWX0xoooollcclooddk0000KKXXXNNXX0xol:,:lloollllccllooooollcc::dXMMMMMMM
+MMMMMMMMMMMMMN0dllllcccooodxkkOO00KKKXXKKOdol:,;llooollcclllllllllc:::l0WMMMMMMM
+MMMMMMMMMMMMMMWOlcccc:coooooddxxxkOO0000Oxoll:,,clooollllllllllccc::::xNMMMMMMMM
+MMMMMMMMMMMMMMMNxccccccooooooooooooddxxddollc;..;clllllloolllccc:::::oXMMMMMMMMM
+MMMMMMMMMMMMMMMW0ocllcclodooooooolllllllcccc:,..';:cllooolllcc::::::l0WMMMMMMMMM
+MMMMMMMMMMMMMMMMNxccllccloodooooooolllcccc::;'.';:llllllllcc:::::::lOWMMMMMMMMMM
+MMMMMMMMMMMMMMMMMKoccllcclodddooooolllllccccc:ccccc::::;;;;;;::ccclOWMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWOlclllc::clooooolllcccccc::::ccc::;,'.',;::cclclOWMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMNxccllcc:;;;::cccccc:::::;;;cccc::;'',;::clllccdNMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWW0lcccccc:;,'.,;::::cc::::;:ccc::;'';:cclllcc:c0MMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMWNKOdcccccc::;;,'',;;:::::::;:ccc::;,,;:cllllcc:cxNMMMMMMMMMMMMMM
+MMMMMMMMMMMWNXXK0xlc::ccccc::;;,''',;:::::;;:cc::;'';ccllllcc::dXMMMMMMMMMMMMMMM
+MMMMMMMMMMNXKK0kocc:::cclllccc::,'',;;;;;;,,;::;;,';clllllcc::l0WMMMMMMMMMMMMMMM
+MMMMMMMWNXK00Oxlc::::::cllllllcc:;,,;;::::;;;::;,';:cllllcc::lkKWMMMMMMMMMMMMMMM
+MMMMMMMKkxkOOxlc:::::::cllllllllc:;,,;;:::::::;,',:cllllcc::lkKXNWMMMMMMMMMMMMMM
+MMMMMMWKkkOkdcc:::::::::cllllcllcc:,,,;;;;;;;;,'';cllllccc:cxKXNNNWWMMMMMMMMMMMM
+MMMMMMXkdoccc::::::c::;:cllllccccc:;,;;;;;;;;;,',:clllcc::lkKKKXXKKNWMMMMMMMMMMM
+MMMMMMKo:::::::::cc::;;::clllccccc:;,,,;;;;;;,'';cccccc::cxKXXKKKKKNMMMMMMMMMMMM
+MMMMMMWOc:::::::cc:;;;:::ccllcccccc:,,,,,,,'''',:ccccc::;;ckXNXKKXXNMMMMMMMMMMMM
+MMMMMMMNx::::cccc:;;;::;::ccccccccc:;,',,,''..';ccccc:::::;:dKXXKKXNWMMMMMMMMMMM
+MMMMMMMKoc::cccc:;;;::::;;::ccccccc:;,'''''..';:cccc::::cc:;;lOXXKKXWMMMMMMMMMMM
+MMMMMMM0l::ccc:;;;:::::::;,;:ccccccc:;''''''',:cccc:;;:clllc:;ckKXXWMMMMMMMMMMMM
+MMMMMMMWOlcc::;,;::::::::;,;::ccccccc:,'.'.',;:ccc:;;;:cllllc::lONMMMMMMMMMMMMMM
+MMMMMMMMW0oldkdc::::::::;;;;;::ccccccc;''''';:ccc:;:::;:clllccco0WMMMMMMMMMMMMMM
+MMMMMMMMMMNKNMW0l::::::;;;;;;;::cccccc:,''',;ccc::::::;;:llcccoONMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMWk:;:::;;;;;;;;,,:cccccc;,'',;::cccc:;;;;;:ccclkKNMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMXo;;;::::::;;;,,;:ccccc:;,',;;:ccccc::ccccclox0XWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMW0l;;:::::::;;;,,;:ccccc::,,;,;::cccccccccldO0KXWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMWOc;::::::::;;;;;::cccccc:,,'',;;:::::cccokKK0XWMMMMMMMMMMMMMMM
+MMMMMMMMMMMMMMMMMMXo;::::::::;;;;;::cccccc:;''..',,;::::clx0KK0XWMMMMMMMMMMMMM*/
+struct MaxFlow {
+    using edge = array<ll, 3>;
+    static const ll INF = 1e10;
+    vector<edge> edges;
+    vector<vector<int>> adj;
+    vector<int> last;
+    vector<int> dist;
+    int n;
+
+    MaxFlow(int n) : n(n) {
+        adj.assign(n, vector<int>());
+    }
+
+    void add_edge(ll u, ll v, ll cap, bool directed = true) {
+        edges.push_back({v, cap, 0LL});
+        adj[u].push_back(edges.size() - 1);
+        edges.push_back({u, directed ? 0LL : cap, 0LL});
+        adj[v].push_back(edges.size() - 1);
+    }
+
+    bool bfs(int s, int t) {
+        dist.assign(n, -1);
+        dist[s] = 0;
+        queue<int> q({s});
+        while (q.size()) {
+            int u = q.front(); q.pop();
+            if (u == t) break;
+
+            for (int &idx : adj[u]) {
+                auto &[v,cap,flow] = edges[idx];
+                if (cap - flow > 0 && dist[v] == -1) {
+                    dist[v] = dist[u] + 1;
+                    q.push(v);
+                }
+            }
+        }
+        return dist[t] != -1;
+    }
+
+    ll dfs(int u, int t, ll f = MaxFlow::INF) {
+        if (u == t || f == 0) return f;
+
+        for (int &i = last[u]; i < adj[u].size(); ++i) {
+            auto &[v, cap, flow] = edges[adj[u][i]];
+            if (dist[v] != dist[u] + 1) continue;
+            if (ll bottle = dfs(v, t, min(f, cap - flow))) {
+                flow += bottle;
+                ll &rev_flow = edges[adj[u][i]^1][2];
+                rev_flow -= bottle;
+                return bottle;
+            }
+        }
+
+        return 0;
+    }
+
+    ll max_flow(int s, int t) {
+        ll mf = 0;
+        while (bfs(s,t)) {
+            last.assign(n, 0);
+            while (ll flow = dfs(s, t)) {
+                mf += flow;
+            }
+        }
+        return mf;
+    }
+};
+const int mxN = 2e5+6;
+const ll MOD  = 1e9+7;
+
+void testCase() {
+    int n;
+    cin >> n;
+
+    int is, g, max_time;
+    cin >> is >> g >> max_time;
+    --is;
+    ++max_time;
+
+    int med;
+    cin >> med;
+    vector<bool> goals(n, 0);
+    for (int i = 0; i < med; ++i) {
+        int u;
+        cin >> u, --u;
+        goals[u] = 1;
+    }
+    int m;
+    cin >> m;
+
+    MaxFlow mf(n * max_time + 2);
+    int s = n * max_time;
+    int t = n * max_time + 1;
+
+    for (int i = 0; i < m; ++i) {
+        ll u, v, p, t;
+        cin >> u >> v >> p >> t;
+        --u, --v;
+
+        for (int j = 0; j < max_time; ++j) {
+            if (j + t >= max_time) continue;
+            // Traversing edge (u, j) -> (v, j + t). Max p people.
+            mf.add_edge(j * n + u, (j + t) * n + v, p);
+        }
+    }
+
+    for (int u = 0; u < n; ++u) {
+        for (int j = 0; j < max_time; ++j) {
+            if (j < max_time - 1)
+                // Stay on the same spot for one time step
+                mf.add_edge(j * n + u, (j + 1) * n + u, MaxFlow::INF);
+
+        }
+        if (goals[u]) {
+            // Reached facility
+            mf.add_edge((max_time - 1) * n + u, t, MaxFlow::INF);
+        }
+    }
+    // Could also do min(max_flow, g)
+    mf.add_edge(s, is, g);
+
+    cout << mf.max_flow(s, t) << endl;
+}
+
+void setIO() {
+    cin.tie(0)->sync_with_stdio(0);
+}
+
+void pre() {
+
+}
+
+int main() {
+    setIO();
+    pre();
+    int t = 1;
+    cin >> t;
+
+    for (int tc = 1; tc <= t; ++tc) {
+        testCase();
+    }
+    return 0;
+}
