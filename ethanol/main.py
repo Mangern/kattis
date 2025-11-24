9bbb95ec27b7535d4517c2bba96f63ef2e368b79
Magnus <magnuseifr@gmail.com>
2025-11-24 21:57:34 +0100

    ethanol

---
+++ ethanol/main.py
@@ -0,0 +1,9 @@
+n=int(input())
+hs=["H"]*n
+bs=["|"]*n
+cs=["C"]*n
+print(f'  {" ".join(hs)}')
+print(f'  {" ".join(bs)}')
+print(f'H-{"-".join(cs)}-OH')
+print(f'  {" ".join(bs)}')
+print(f'  {" ".join(hs)}')
