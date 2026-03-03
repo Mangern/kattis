ef9b7217dfc7efe1939c155f05757c028542854b
Magnus <magnuseifr@gmail.com>
2026-03-03 13:23:54 +0100

    grass

--- grass/main.py
+++ grass/main.py
@@ -12,26 +12,27 @@ while ptr < len(lines):
     segs = []
 
     for spr in sprs:
-        if 4 * spr[1] * spr[1] <= w * w:
+        x, r = spr
+        if 2 * r <= w:
+            # Not covering anything useful
             continue
         
-        rs = spr[1] * spr[1] - (w*w/4)
+        # r ^2 = (w/2) ^2 + dx ^ 2
+        rs = r * r - (w*w/4)
         rs = sqrt(rs)
-        segs.append((max(-1.0,spr[0]-rs), min(l+1,spr[0]+rs)))
+        segs.append((max(-1.0,x-rs), min(l+1,x+rs)))
 
     segs.sort()
     L = 0
     R = 0
     ans = 0
     i = 0
-    while i < len(segs):
-        R = -1
+    while i < len(segs) and L < l:
         while i < len(segs) and segs[i][0] <= L:
             if R < segs[i][1]:
                 R = segs[i][1]
-            else:
-                i += 1
-        if R == -1:
+            i += 1
+        if R <= L:
             break
         L = R
         ans += 1
