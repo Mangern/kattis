fed8f687bf4d2ded10255b1d612ef482ae28e561
Magnus <magnuseifr@gmail.com>
2026-05-16 18:31:46 +0200

    sourcetosink

---
+++ sourcetosink/main.rs
@@ -0,0 +1,51 @@
+fn main() {
+    let mut io = IO::new(std::io::stdin(), std::io::stdout());
+
+    let l: i64 = io.read();
+    let w: i64 = io.read();
+    let v: i64 = io.read();
+
+    let empty_count = (l * w + v - 1) / v;
+
+    // For all except last:
+    // open 1, close 1, open 2, close 2 = 4 ops.
+    // For last: open 1, close 1, open 2 = 3 ops
+
+    let ans = 4 * (empty_count - 1) + 3;
+
+    println!("{ans}");
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: ToString>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write_all(s.to_string().as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+}
