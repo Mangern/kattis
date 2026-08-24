e4ed53b961d83ebdf0d6d9ee67621317183dfc9d
Magnus <magnuseifr@gmail.com>
2026-08-24 18:39:51 +0200

    eloratings

---
+++ eloratings/main.py
@@ -0,0 +1,11 @@
+e=int(input())
+if e >= 2700:
+    print("Super grandmaster")
+elif e >= 2500:
+    print("Grandmaster")
+elif e >= 2400:
+    print("International grandmaster")
+elif e >= 1000:
+    print("Amateur")
+else:
+    print("Invalid")
